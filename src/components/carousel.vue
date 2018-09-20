576af9c076c55c612602868ff9dfab82b6770b0e
cuiguochao <cuiguochao@saotx.cn>
2018-09-20 19:56:42 +0800

    方便合分支

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -20,7 +20,7 @@
 
         <mt-swipe :auto="4000">
             <mt-swipe-item v-for="(item, idx) in mybanner" :key="idx">
-                <a :href="'/views/activity/' + path[item.form] + '?actCode=' + item.actCode" class="bannera">
+                <a :href="'/yx-page/views/activity/' + path[item.form] + '?actCode=' + item.actCode" class="bannera">
                     <img :src="item.banner" alt="">
                 </a>
             </mt-swipe-item>
@@ -65,7 +65,6 @@
             }
         },
         created() {
-            console.log(this.path)
             // this.play();
             // this.ULwidth = 750 * (this.banner.length + 2);
         },
